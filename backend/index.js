e4be3e4cfedde13c7435fc3043f4f9d0ae023a66
Aditya Madan <adityamadan01@gmail.com>
2026-01-03 19:43:45 +0530

    backend

---
+++ backend/index.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const cors = require("cors");
+const zod = require("zod");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const icaoSchema = zod.object({
+  icao: zod.string().length(4)
+});
+
+app.get("/data", async (req, res) => {
+  let payload = req.body;
+  if (!payload || Object.keys(payload).length === 0) {
+    payload = { icao: req.query.icao }; // Extract from ?icao=VIDP
+  }
+  const validatedPayload = icaoSchema.safeParse(payload);
+
+  if (!validatedPayload.success) {
+    res.status(411).json({
+      msg: "Please Verify Inputs ",
+    });
+  }
+  const icao = payload.icao;
+  const response = await fetch(
+    `https://aviationweather.gov/api/data/metar?ids=${icao}&format=json`
+  );
+  const jsonResponse = await response.json();
+  if (jsonResponse.length == 0 || !jsonResponse) {
+    res.status(401).json({
+      msg: "No METAR found, please check the inputs",
+    });
+    return;
+  }
+  const metar = jsonResponse[0];
+  const stationName = metar.name;
+  const rawMetar = metar.rawOb;
+  const temp = metar.temp;
+  const vis = metar.visib;
+  const cloudsFew = metar.clouds[0].base;
+  const cloudsSct = metar.clouds[1].base;
+  const wind = `${metar.wdir}@${metar.wspd}`;
+
+  const runwayResponse = await fetch(
+    `https://aviationweather.gov/api/data/airport?ids=${icao}&format=json`
+  );
+  const jsonRunwayResponse = await runwayResponse.json();
+  if (jsonRunwayResponse.length == 0 || !jsonRunwayResponse) {
+    res.status(401).json({
+      msg: "No data found, please check the inputs",
+    });
+    return;
+  }
+  const info = jsonRunwayResponse[0];
+  const lat = info.lat;
+  const long = info.long;
+  const runways = info.runways;
+
+  res.status(200).json({
+    stationName,
+    rawMetar,
+    temp,
+    vis,
+    cloudsFew,
+    cloudsSct,
+    wind,
+    lat,
+    long,
+    runways,
+  });
+
+});
+
+
+app.listen(3000,()=>{
+    console.log("Server running on port 3000");
+})
